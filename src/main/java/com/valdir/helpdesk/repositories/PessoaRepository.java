73c74c49c30e05c32df6d9ebfe33ab8c6f61d0ad
Luiz Fernando <LUIZFERNANDOJUNIOR9999@GMAIL.COM>
2024-10-21 17:28:30 -0300

    Aula 15 - Exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/valdir/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/valdir/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,15 @@
 package com.valdir.helpdesk.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.valdir.helpdesk.domain.Pessoa;
 import com.valdir.helpdesk.domain.Tecnico;
 
 public interface PessoaRepository extends JpaRepository<Tecnico, Integer>{
 
+		Optional<Pessoa> findByCpf(String cpf);
+		Optional<Pessoa> findByEmail(String email);
 }
