036e0bcc0a1672f013c37d0b41c8bebe652a9cff
Luiz Fernando <LUIZFERNANDOJUNIOR9999@GMAIL.COM>
2024-10-23 22:25:40 -0300

    Aula 21 - Implementado findAll para Chamado

--- src/main/java/com/valdir/helpdesk/services/ChamadoService.java
+++ src/main/java/com/valdir/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,8 @@ public class ChamadoService {
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
-	
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
+
 }
